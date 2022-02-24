80c62e250122a9f68dd8869043f5f5a90493eff2
“ranjani <ranjani.murugesankarunagaran@stud.tu-darmstadt.de>
2022-02-24 11:51:52 +0100

    Initial commit : Demo of pure functions, filter() and map()

---
+++ PureFunctions.py
@@ -0,0 +1,8 @@
+#multiply by 2 and return
+def multiby2(li):
+    new_list = []
+    for item in li:
+        new_list.append(item*2)
+    return new_list
+
+print(multiby2([1,2,3]))
---
+++ filter.py
@@ -0,0 +1,7 @@
+#return only odd numbers
+my_list = [1,2,3]
+def only_odd(item):
+    return item%2!=0
+
+print(list(filter(only_odd, my_list)))
+print(my_list)
